766b17b8410fea9812f99beb5926ce1082ba787f
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-04-16 22:39:02 +0800

    Create 0027 Remove Element.cpp

---
+++ code/0027 Remove Element.cpp
@@ -0,0 +1,10 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int j=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=val){
+            nums[j]=nums[i];
+            j++;
+        }
+    }
+    return j;
+}
