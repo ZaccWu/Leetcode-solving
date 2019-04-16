c121273959f490c4558767d5b0358a3e1c0780cd
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-04-16 22:37:51 +0800

    Create 0007 Reverse Integer.cpp

---
+++ code/0007 Reverse Integer.cpp
@@ -0,0 +1,9 @@
+int reverse(int x) {
+    long result = 0;
+    while(x!=0)
+    {
+        result = result*10 + x%10;
+        x/=10;
+    }
+    return (result > INT_MAX || result < INT_MIN)? 0 : result;
+}
