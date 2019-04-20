8637584771196e5f5a94a8650738c2e113f45ac6
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-04-20 13:19:53 +0800

    Create 0739 Daily Temperatures.py

---
+++ 0739 Daily Temperatures.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        ans = [0]*len(T)
+        stack = []
+        for i, t in enumerate(T):
+            while(stack and T[stack[-1]] < t):
+                cur = stack.pop()
+                ans[cur] = i - cur
+            stack.append(i)
+        return ans
