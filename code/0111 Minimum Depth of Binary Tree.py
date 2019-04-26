c81ae77a88bb9fe4d11974de2d7bf45c0414ae79
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-04-26 09:58:51 +0800

    Create 0111 Minimum Depth of Binary Tree.py

---
+++ code/0111 Minimum Depth of Binary Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+#DFS
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if None in [root.left, root.right]:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+#BFS      
+    def minDepth1(self, root):
+        if not root:
+            return 0
+        queue = collections.deque([(root, 1)])
+        while queue:
+            node, level = queue.popleft()
+            if node:
+                if not node.left and not node.right:
+                    return level
+                else:
+                    queue.append((node.left, level+1))
+                    queue.append((node.right, level+1))
+        
