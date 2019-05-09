cd7c22cdfa6d3e07d6a896a27741d006f8911a2d
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-05-09 10:11:17 +0800

    Update 0406 Queue Reconstruction by Height.py

--- code/0406 Queue Reconstruction by Height.py
+++ code/0406 Queue Reconstruction by Height.py
@@ -5,7 +5,9 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res=[]
+        # For the first component, we use descending order, for the second component, we use ascending order.
         people = sorted(people, key = lambda x: (-x[0], x[1]))
+        # The usage of 'insert' function: insert(index,object)
         for p in people:
             res.insert(p[1], p)
         return res
