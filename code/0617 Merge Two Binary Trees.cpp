ad565cdbc69f9c9099a639971e2f3470bd4c9df6
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-05-28 19:28:31 +0800

    Create 0617 Merge Two Binary Trees.cpp

---
+++ code/0617 Merge Two Binary Trees.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2){
+    if(!t1) return t2;
+    if(t2){
+        t1->val+=t2->val;
+        t1->left=mergeTrees(t1->left,t2->left);
+        t1->right=mergeTrees(t1->right,t2->right);
+    } 
+    return t1;  
+}
+
