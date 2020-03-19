c73d3e5b82d22531e6679c9a6bc8d0e19c55791c
Ziyue Wu <37011471+Tinky2013@users.noreply.github.com>
2020-03-19 13:16:40 +0800

    Update and rename 0206 Reverse Linked List.cpp to 0206 反转链表.cpp

--- C&C++/0206 反转链表.cpp
+++ C&C++/0206 反转链表.cpp
@@ -1,3 +1,16 @@
+/*
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+限制：
+0 <= 节点个数 <= 5000
+*/
+
+//反转链表，这题是基本功
+//递归和迭代两种，递归要判断边界，迭代关键在于三指针
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -23,7 +36,7 @@ struct ListNode* reverseList(struct ListNode* head) {
     while(head)
     {
         next=head->next;
-        head->next=prev;
+        head->next=prev;    //箭头的反转
         prev=head;
         head=next;
     }
