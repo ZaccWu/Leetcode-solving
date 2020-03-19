971b5039a8239e466d0536c45032e754b80c714f
Ziyue Wu <37011471+Tinky2013@users.noreply.github.com>
2020-03-19 13:42:44 +0800

    Update and rename 0226 Invert Binary Tree.cpp to 0226 翻转二叉树.cpp

--- C&C++/0226 翻转二叉树.cpp
+++ C&C++/0226 翻转二叉树.cpp
@@ -1,3 +1,23 @@
+/*
+翻转一棵二叉树。
+
+示例：
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -6,7 +26,7 @@
  *     struct TreeNode *right;
  * };
  */
-
+//基本功，此题最简洁的方法就是递归
 struct TreeNode* invertTree(struct TreeNode* root){
     if(!root) return root;
     struct TreeNode *L=invertTree(root->left);
